92dd2bcb3322c508d845bc98b7bedcef4fe9a0a0
Kevin-gram <k.nyiringan@alustudent.com>
2025-10-20 17:48:25 +0200

    work on report sharing to all users

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,7 +6,7 @@ export const fetchBranches = async () => {
 import { User, Loan, Contribution } from "../types";
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://community-saver-quyb.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE,
